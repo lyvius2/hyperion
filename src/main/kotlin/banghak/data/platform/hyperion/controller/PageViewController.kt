4a4e5294b49a5e8e8b6ca74425f6fb61bf738f4b
walter.hwang <lyvius2@naver.com>
2026-06-16 21:28:42 +0900

    패키지 변경

--- src/main/kotlin/banghak/data/platform/hyperion/controller/PageViewController.kt
+++ src/main/kotlin/banghak/data/platform/hyperion/controller/PageViewController.kt
@@ -1,4 +1,4 @@
-package banghak.data.platform.hyperion.web
+package banghak.data.platform.hyperion.controller
 
 import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.GetMapping
